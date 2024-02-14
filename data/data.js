313b04d6f7ba2cbd37b1e5bfc3185fb69afd81ac
vinny-mze <davinciian5@gmail.com>
2024-02-14 13:10:44 +0200

    Updated products array

--- data/data.js
+++ data/data.js
@@ -37,7 +37,7 @@ export const products = [{
     count: 56
   },
   description: "Renowned for its rich composition of monounsaturated fats and antioxidants, our macadamia oil is a powerhouse for skin and hair health. Experience deep hydration as the oil absorbs quickly, leaving your skin soft and supple. Its lightweight texture makes it an ideal choice for all skin types.",
-  category: "Soy Candles"
+  category: "Essential Oils"
 },
 {
   id: "p004",
@@ -50,6 +50,71 @@ export const products = [{
     count: 2197
   },
   description: "Awaken your senses with the vibrant aroma of our Pure Lemon Essential Oil, extracted from the zesty peels of sun-kissed lemons. Immerse yourself in the invigorating and refreshing essence that encapsulates the purest form of this citrus treasure.", 
+  category: "Essential Oils"
+},
+{
+  id: "p005",
+  image:"../images/store/dahlia-soap.jpg",
+  name: "Dahlia Bloom Organic Soap",
+  quantity: "16g",
+  priceCents: 800,
+  rating: {
+    stars: 4.8,
+    count: 3012
+  },
+  description: "Indulge in the gentle caress of nature with our Dahlia Bloom Organic Soap, handcrafted to perfection using the vibrant petals of dahlia flowers. Envisioned and created with utmost care, this soap is a harmonious blend of purity and luxury.", 
   category: "Organic Soaps"
+},
+{
+  id: "p006",
+  image:"../images/store/lavendar-soap.jpg",
+  name: "Lavender Bliss Organic Soap",
+  quantity: "24g",
+  priceCents: 645,
+  rating: {
+    stars: 4.7,
+    count: 5948
+  },
+  description: "Experience a moment of tranquility with our Lavender Bliss Organic Soap, lovingly handcrafted with the essence of lavender and enriched with the goodness of organic ingredients. Immerse yourself in the calming aroma of lavender fields and indulge in the luxurious lather of this exquisite soap.", 
+  category: "Organic Soaps"
+},
+{
+  id: "p007",
+  image:"../images/store/macadamia-soap.jpg",
+  name: "Macadamia Dream Organic Soap",
+  quantity: "16g",
+  priceCents: 250,
+  rating: {
+    stars: 5,
+    count: 817
+  },
+  description: "Embrace the lavish indulgence of nature with our Macadamia Dream Organic Soap, meticulously handcrafted to lavish your skin with the richness of macadamia nuts. Immerse yourself in a luxurious bathing experience as the soothing aroma of macadamia transports you to a blissful state of relaxation.", 
+  category: "Organic Soaps"
+},
+{
+  id: "p008",
+  image:"../images/store/allium-candle.jpg",
+  name: "Allium Aura Soy Candle",
+  quantity: "200ml",
+  priceCents: 280,
+  rating: {
+    stars: 4,
+    count: 590
+  },
+  description: "Illuminate your space with the enchanting allure of our Allium Aura Soy Candle, crafted with care to capture the essence of the beloved Allium Ornamental flower. Each flicker of the flame releases a delicate fragrance that transports you to a garden in full bloom, where the air is filled with the sweet scent of spring.", 
+  category: "Soy Candles"
+},
+{
+  id: "p009",
+  image:"../images/store/eucalyptus-candle.jpg",
+  name: "Eucalyptus Breeze Soy Candle",
+  quantity: "200ml",
+  priceCents: 250,
+  rating: {
+    stars: 5,
+    count: 817
+  },
+  description: "Experience the revitalizing power of nature with our Eucalyptus Breeze Soy Candle, carefully crafted to infuse your space with the refreshing aroma of eucalyptus. Envelop yourself in the crisp, invigorating scent of eucalyptus leaves as they dance on a gentle breeze, transporting you to the heart of a lush forest sanctuary.", 
+  category: "Soy Candles"
 }
 ]
--- scripts/detail.js
+++ scripts/detail.js
@@ -18,54 +18,48 @@ const loadTemplate = () => {
 loadTemplate();
 const initApp = () => {
   let productId = new URLSearchParams(window.location.search).get('id');
-  let thisProduct = products.filter(value => value.id == productId)[0];
+  let thisProduct = products.find(product => product.id == productId);
   
-    if(!thisProduct){
-        window.location.href = "/";
-}
-let detail = document.querySelector('.detail')
+  if (!thisProduct) {
+    window.location.href = "/";
+    return; // Added to exit the function if the product is not found
+  }
 
-detail.querySelector('.image img').src = thisProduct.image;
-detail.querySelector('.name').innerText = thisProduct.name;
+  let detail = document.querySelector('.detail');
 
-detail.querySelector('.price').innerText = 'R' + thisProduct.priceCents;
-detail.querySelector('.quantity').innerText = thisProduct.quantity
-detail.querySelector('.rating').innerText = thisProduct.rating.stars+ '('+thisProduct.rating.count+')'
-detail.querySelector('.description').innerText = thisProduct.description;
-detail.querySelector('.addCart').dataset.id = productId;
+  // Update detail page with product information
+  detail.querySelector('.image img').src = thisProduct.image;
+  detail.querySelector('.name').innerText = thisProduct.name;
+  detail.querySelector('.price').innerText = 'R' + thisProduct.priceCents;
+  detail.querySelector('.quantity').innerText = thisProduct.quantity;
+  detail.querySelector('.rating').innerText = thisProduct.rating.stars + '(' + thisProduct.rating.count + ')';
+  detail.querySelector('.description').innerText = thisProduct.description;
+  detail.querySelector('.addCart').dataset.id = productId;
 
-let productsHTML ='';
+  // Filter products by category
+  let relatedProducts = products.filter(product => product.category === thisProduct.category && product.id !== productId);
 
-products.filter((value) => value.id !=productId).forEach((product) => {
-  productsHTML +=`
-  <div class="product-container">
-            <a href="/detail.html?id=${product.id}">
+  let productsHTML = '';
+  relatedProducts.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <a href="/detail.html?id=${product.id}">
           <div class="product-image-container">
             <img src="${product.image}" class="product-image">
           </div>
-          </a>
-          <div class="product-description">
-            <div class="product-name">
-              ${product.name}
-            </div>
-            <div class="product-spacer"></div>
-            <div class="product-quantity">
-              ${product.quantity}
-            </div>
-            <div class="product-price">
-              R${product.priceCents.toFixed(2)}
-            </div>
-            
-            <div class="product-rating"> 
-              <img src="images/icons/star-512.png" class="ratingImage">${product.rating.stars} (${product.rating.count})
-            </div>
-            
-            <a class="quickView addCart" data-id="${product.id}">
-              Add To bag
-            </a>
-            
+        </a>
+        <div class="product-description">
+          <div class="product-name">${product.name}</div>
+          <div class="product-spacer"></div>
+          <div class="product-quantity">${product.quantity}</div>
+          <div class="product-price">R${product.priceCents.toFixed(2)}</div>
+          <div class="product-rating">
+            <img src="images/icons/star-512.png" class="ratingImage">${product.rating.stars} (${product.rating.count})
           </div>
-        </div>`
-});
-document.querySelector('.listProduct').innerHTML = productsHTML;
+          <a class="quickView addCart" data-id="${product.id}">Add To bag</a>
+        </div>
+      </div>`;
+  });
+
+  document.querySelector('.listProduct').innerHTML = productsHTML;
 }
